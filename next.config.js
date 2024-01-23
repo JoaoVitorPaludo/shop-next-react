ff76a5bfaebe0d1e8dc4fe56069d6416868ce1f4
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-01-23 12:53:16 -0300

    feat: fazendo a conexão com a api do stripe

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-   
+  images: {
+    remotePatterns: [
+      {
+        hostname: "files.stripe.com",
+      },
+    ],
   },
 };
 
