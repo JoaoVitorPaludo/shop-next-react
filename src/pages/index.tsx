52ee9cc706e8a7af39c320275e57af81a82b0f90
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-01-29 12:59:46 -0300

    feat: implementando a sintese de requisições de api no next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,24 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
+
 import { useKeenSlider } from "keen-slider/react";
 
+import { stripe } from "../lib/stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import "keen-slider/keen-slider.min.css";
-import Image from "next/image";
-import { stripe } from "../lib/stripe";
-import { GetStaticProps } from "next";
 import Stripe from "stripe";
-import Link from "next/link";
+import Image from "next/image";
 
 interface HomeProps {
   products: {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[];
 }
+
 export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
@@ -29,7 +31,11 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`}>
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            prefetch={false}
+          >
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
@@ -49,6 +55,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
+
   const products = response.data.map((product) => {
     const price = product.default_price as Stripe.Price;
 
@@ -62,10 +69,11 @@ export const getStaticProps: GetStaticProps = async () => {
       }).format(price.unit_amount! / 100),
     };
   });
+
   return {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2,
+    revalidate: 60 * 60 * 2, // 2 hours,
   };
 };
